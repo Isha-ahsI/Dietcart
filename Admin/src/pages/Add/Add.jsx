1dde0469d4403cbae960a94bfe3ead0c075dd1f4
Isha-ahsI <isha142005@gmail.com>
2025-02-15 19:25:57 +0530

    create add item option in admin panel

--- Admin/src/pages/Add/Add.jsx
+++ Admin/src/pages/Add/Add.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 import "./Add.css"
 import { assets } from '../../assets/assets'
+import axios from 'axios'
+import {useFormik} from "formik"
+import { toast } from 'react-toastify'
 
 
 const Add = () => {
+
+  const url="http://localhost:4000";
   const [image,setImage] = useState(false);
   const [data,setData] = useState({
     name:"",
@@ -20,43 +25,87 @@ const Add = () => {
 
   
   
-  const onSubmitHandler = async (event) => {
-    // event.preventDefault();
-    // const formData = new FormData();
-    // formData.append("name",data.name)
-    // formData.append("description",data.description)
-    // formData.append("price",Number(data.price))
-    // formData.append("category",data.category)
-    // formData.append("image",image)
+  const onSubmitHandler = async(event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("image",image)
+    formData.append("name",data.name)
+    formData.append("description",data.description)
+    formData.append("price",Number(data.price))
+    formData.append("category",data.category)
+    const response=await axios.post(`${url}/api/food/add`,formData);
+    if (response.data.success) {
+      setData({
+        name:"",
+        description:"",
+        category:"salad",
+        price:""
+      })
+      setImage(false)
+      toast.success(response.data.message)
+    }
+    else{
+      toast.error(response.data.message)
+    }
   }
   
+// const formik = useFormik({
+//   initialValues:{
+//     name:"",
+//     description:"",
+//     category:"",
+//     price:""
+//   },onSubmit:async(values)=>{
+//     try{
+//       const res=await fetch("http://localhost:3002/add",{
+//         method:"post",
+//         headers:{
+//           "contect-type":"application/json",
+//         },
+//         body:JSON.stringify(values),
+//       });
+//       if(!res.ok){
+//         throw new Error(`HTTP error! status:${res.status}`);
+//       }
+//       const result=await res.json();
+//       alert ("successfully");
+
+//     }catch(error){
+//       alert("error:"+Error.message);
+//       console.error("error:",error);
+//     }
+//   }
+// });
+
   return (
     <div className='add'>
-      <form className="flex-col" onSubmit={onSubmitHandler}>
+      <form className="flex-col"  method="post" onSubmit={onSubmitHandler}>
        
+        
+        
         <div className='add-img-upload flex-col'>
          <p>Upload Image</p>
          <label htmlFor='image'>
             <img src={image?URL.createObjectURL(image):assets.upload_icon} alt=''/>
          </label>
-         <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden required/>
+         <input onChange={(e)=>setImage(e.target.files[0])} type="file" name='image' id="image" hidden required/>
         </div>
         
         <div className="add-product-name flex-col">
           <p>Product Name</p>
-          <input onChange={onChangeHandler} value={data.name} type='text' name='name' placeholder='Type Here'/>
+          <input onChange={onChangeHandler} value={data.name}  type='text' name='name' placeholder='Type Here' required/>
         </div>
 
         <div className="add-product-description flex-col">
           <p>Product Description</p>
-          <textarea onChange={onChangeHandler} value={data.description} name='description' rows="6" placeholder='Write Content here'></textarea>
+          <textarea onChange={onChangeHandler} value={data.description}  name='description' rows="6" placeholder='Write Content here' required></textarea>
         </div>
 
         <div className="add-category-price">
           
           <div className="add-category flex-col">
             <p>Product category</p>
-            <select onChange={onChangeHandler} name="category">
+            <select onChange={onChangeHandler}   name="category">
                 <option value="Egg Plate">Egg Plate</option>
                 <option value="Smoothies">Smoothies</option>
                 <option value="Salad">Salad</option>
@@ -69,7 +118,7 @@ const Add = () => {
 
           <div className="add-price">
             <p>Product Price</p>
-            <input onChange={onChangeHandler} value={data.price} type="text" name='price' placeholder='Rs.'/>        
+            <input onChange={onChangeHandler} value={data.price}  type="text" name='price' placeholder='Rs.' required/>        
           </div>
 
         </div>
--- Admin/src/pages/List/List.jsx
+++ Admin/src/pages/List/List.jsx
@@ -3,8 +3,19 @@ import "./List.css"
 
 const List = () => {
   return (
-    <div>
-      
+    <div className='list add flex-col'>
+      <h2>All Foods List</h2>
+      <div className='list-tabel'>
+        <div className='list-table-format title'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Description</b>
+          <b>Categorys</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+
+      </div>
     </div>
   )
 }
