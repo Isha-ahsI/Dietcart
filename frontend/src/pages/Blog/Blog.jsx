0a786c5b72fdd04c2d05c5b09fb04bdee93c92e1
Isha-ahsI <isha142005@gmail.com>
2025-03-31 22:33:46 +0530

    change css for blog page

--- frontend/src/pages/Blog/Blog.jsx
+++ frontend/src/pages/Blog/Blog.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import './Blog.css'
-import { blogPosts } from '../../assets/assets'
+import React, { useState } from 'react';
+import './Blog.css';
+import { blogPosts } from '../../assets/assets';
 
 function Blog() {
+  const [visibleDescription, setVisibleDescription] = useState(null);
+
+  const toggleDescription = (index) => {
+    setVisibleDescription(visibleDescription === index ? null : index);
+  };
+
   return (
     <div className='blog-container'>
-     
-        {blogPosts.map((item,index)=>{
-            return(
-                <div key={index} className='blog-cart'>
-                    
-                        <img src={item.image}/>
-                        <div className='blog-info'>
-                            <h2>{item.title}</h2>
-                            <p>{item.description}</p>
-                        </div>
-                    
-                    
-                </div>
-            )
-        })}
-       
-      </div>
-   
-      
-
-  )
+      {blogPosts.map((item, index) => (
+        <div key={index} className='blog-cart'>
+          <img src={item.image} alt={item.title} />
+          <div className='blog-info'>
+            <h2>{item.title}</h2>
+            <p className='blog-date'>{item.date}</p>
+            <button 
+              className='show-description-btn' 
+              onClick={() => toggleDescription(index)}
+            >
+              {visibleDescription === index ? "Read Less" : "Read More"}
+            </button>
+            {visibleDescription === index && <p className="blog-description">{item.description}</p>}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
